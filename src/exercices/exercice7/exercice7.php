55df84ccb380b30eeeda1f32a0f743511bc4b08c
ElifKaya <elif.kaya@studentfr.ch>
2025-01-28 10:47:51 +0100

    dd

--- src/exercices/exercice7/exercice7.php
+++ src/exercices/exercice7/exercice7.php
@@ -1,14 +1,14 @@
 <?php
-	$bdd = new PDO('mysql:host=localhost;dbname=nomDB', 'root', 'pwd');
-	$reponse = …;
-	
-	
-	while (…)
-	{
-	
-	        …
+$bdd = new PDO('mysql:host=localhost;dbname=nomDB', 'root', 'pwd');
+$reponse = $bdd->query('select pk_jeux_video, titre from jeux_video');
 
-	
-	}
-	$reponse->closeCursor();
+
+while ($row = $response->fetch())
+{
+
+	echo $row['titre'] . $row['pk_jeux_video'];
+
+
+}
+$reponse->closeCursor();
 ?>
