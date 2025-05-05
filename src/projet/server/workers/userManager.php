24cb380f66b1eea1b1f499a0e85af56d21eac257
elif-mee <elif.kaya@studentfr.ch>
2025-05-05 10:23:01 +0200

    Create userManager.php

---
+++ src/projet/server/workers/userManager.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @author Kaya
+ */
+
+class UserManager
+{
+    private $dbManager;
+
+    public function __construct()
+    {
+        $this->dbManager = new DBUserManager();
+    }
+
+    /**
+     * Vérifie les identifiants de l'utilisateur.
+     *
+     * @param string $email
+     * @param string $password
+     * @return User|false Retourne l'objet User si les identifiants sont valides, false sinon.
+     */
+    public function checkCredentials($email, $password)
+    {
+        $user = $this->dbManager->checkLogin($email);
+        if ($user) {
+            if ($password === $user->getPassword()) {
+                return $user;
+            }
+        }
+        return false;
+    }
+}
